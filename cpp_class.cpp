d6f9fe186708025a568508946e6a15222485425d
rovis43 <60111390+rovis43@users.noreply.github.com>
2023-07-27 12:13:57 -0300

    First Commit

---
+++ cpp_class.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+consteval int get_value() {
+    return 3;
+}
+
+int main() {
+    std::cout << "Hello World! \n";
+    std::cout << "Hello World!" << std::endl;
+    
+    return 0;
+}   
